01310ea6fb85ebd2bb057700fe15a39040e38d08
Alberto Hernandez Lopez <wildcards@gmail.com>
2020-12-01 16:53:41 -0600

    Drivetrian motor numbers changed

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -16,21 +16,21 @@ import frc.robot.commands.TankDrive;
 
 public class DriveTrain extends SubsystemBase {
   /* Declares and initializes motors */
-  private final VictorSPX motorLeft9 = new VictorSPX(Constants.MOTOR_9);
-  private final VictorSPX motorLeft10 = new VictorSPX(Constants.MOTOR_10);
-  private final VictorSPX motorLeft11 = new VictorSPX(Constants.MOTOR_11);
-  private final VictorSPX motorRight1 = new VictorSPX(Constants.MOTOR_1);
-  private final VictorSPX motorRight2 = new VictorSPX(Constants.MOTOR_2);
-  private final VictorSPX motorRight3 = new VictorSPX(Constants.MOTOR_3);
+  private final VictorSPX motorLeft1 = new VictorSPX(Constants.MOTOR_1);
+  private final VictorSPX motorLeft2 = new VictorSPX(Constants.MOTOR_2);
+  private final VictorSPX motorLeft3 = new VictorSPX(Constants.MOTOR_3);
+  private final VictorSPX motorRight14 = new VictorSPX(Constants.MOTOR_14);
+  private final VictorSPX motorRight15 = new VictorSPX(Constants.MOTOR_15);
+  private final VictorSPX motorRight16 = new VictorSPX(Constants.MOTOR_16);
 
   /*
    * This method sets the speed of the left motors (returns a double ranging from
    * -1 to 1)
    */
   public void setLeftMotors(final double speed) {
-    motorLeft9.set(ControlMode.PercentOutput, -speed);
-    motorLeft10.set(ControlMode.PercentOutput, -speed);
-    motorLeft11.set(ControlMode.PercentOutput, -speed);
+    motorLeft1.set(ControlMode.PercentOutput, -speed);
+    motorLeft2.set(ControlMode.PercentOutput, -speed);
+    motorLeft3.set(ControlMode.PercentOutput, -speed);
   }
 
   /*
@@ -38,9 +38,9 @@ public class DriveTrain extends SubsystemBase {
    * -1 to 1)
    */
   public void setRightMotors(final double speed) {
-    motorRight1.set(ControlMode.PercentOutput, speed);
-    motorRight2.set(ControlMode.PercentOutput, speed);
-    motorRight3.set(ControlMode.PercentOutput, speed);
+    motorRight14.set(ControlMode.PercentOutput, speed);
+    motorRight15.set(ControlMode.PercentOutput, speed);
+    motorRight16.set(ControlMode.PercentOutput, speed);
   }
 
   @Override
