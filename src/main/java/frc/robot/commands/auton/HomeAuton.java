cd825f6494949a560486e1e0fcdc1c0e9df5e3c2
Xarkaidier <penguinsilk176@gmail.com>
2021-03-30 12:32:59 -0500

    added search auto code

--- src/main/java/frc/robot/commands/auton/HomeAuton.java
+++ src/main/java/frc/robot/commands/auton/HomeAuton.java
@@ -25,7 +25,6 @@ public class HomeAuton extends SequentialCommandGroup {
     //autoCmdManual.Forward(12,false);
     // Add your commands in the super() call, e.g.
     Slalom();
-    //Slalom();
 
   }
 
@@ -58,7 +57,7 @@ public class HomeAuton extends SequentialCommandGroup {
     
     public void Slalom(){
 
-    autoCmdManual.Forward(1.5,false); //Forward out of the gate, Start at 45 Degrees
+    autoCmdManual.Forward(1.5,false); //Forward out of the gate
     
     autoCmdManual.Left(90,false); // Long run 1
     autoCmdManual.Forward(3.5,false);
@@ -66,7 +65,8 @@ public class HomeAuton extends SequentialCommandGroup {
     autoCmdManual.Right(80,false); //Box D10
     autoCmdManual.Forward(10.5,false);
     autoCmdManual.Right(80,false);
-    /*autoCmdManual.Forward(.35,false);
+    autoCmdManual.Forward(5.3,false);
+    /*
     autoCmdManual.Left(.5);
     autoCmdManual.Forward(.9,false);
     autoCmdManual.Left(.3);
@@ -83,8 +83,8 @@ public class HomeAuton extends SequentialCommandGroup {
     autoCmdManual.Forward(100,.4);*/ // Leaving Slalom
     }
     
-    /*public void Bounce(){
-    autoCmdManual.Forward(1); //Target A3
+    public void Bounce(){
+    /*autoCmdManual.Forward(1); //Target A3
     autoCmdManual.Left(90);
     autoCmdManual.Forward(1); //touch
     autoCmdManual.Backward(1);
@@ -105,10 +105,36 @@ public class HomeAuton extends SequentialCommandGroup {
     autoCmdManual.Forward(1); //touch
     autoCmdManual.Backward(1);
     autoCmdManual.bLeft(90); 
-    autoCmdManual.Backward(1); // Exit Bounce
+    autoCmdManual.Backward(1); // Exit Bounce*/
+    }
     
-    }*/
-    
-    
+    public void SearchA(){ //Red and blue seach patten A
+      //Setup centered on 12.5 feet
+      autoCmdManual.Forward(22,false);
+      autoCmdManual.Right(90,false);
+
+      autoCmdManual.Forward(10,false);
+      autoCmdManual.Right(90,false);
+
+      autoCmdManual.Forward(15,false);
+      autoCmdManual.Right(90,false);
+
+      autoCmdManual.Forward(7.5,false);
+      autoCmdManual.Right(90,false);
+
+      autoCmdManual.Forward(12.5,false);
+      autoCmdManual.Right(90,false);
+      
+      autoCmdManual.Forward(5,false);
+      autoCmdManual.Right(90,false);
+
+      autoCmdManual.Forward(15,false);
+
+      autoCmdManual.Forward(25,true);
+
+    }
+    public void SearchB(){ //Red and Blue seach patten B
+      
+    }
 }
 
